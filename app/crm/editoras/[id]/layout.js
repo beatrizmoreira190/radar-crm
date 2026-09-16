d7cf6d3bdea33664d1a6338b186866645c951c21
beatrizmoreira190 <beatrizmoreira190@gmail.com>
2026-09-16 18:29:32 -0300

    Funções comerciais e materiais por link (#33)
    Separa níveis de acesso de funções operacionais e adiciona materiais comerciais por link nas fichas das editoras, com RLS e sem uploads de arquivos.

--- app/crm/editoras/[id]/layout.js
+++ app/crm/editoras/[id]/layout.js
@@ -1,5 +1,6 @@
+import PublisherMaterialsMount from '@/components/PublisherMaterialsMount';
 import PublisherRadarDetailMount from '@/components/PublisherRadarDetailMount';
 
 export default function PublisherDetailLayout({children}){
-  return <>{children}<PublisherRadarDetailMount/></>;
+  return <>{children}<PublisherMaterialsMount/><PublisherRadarDetailMount/></>;
 }
