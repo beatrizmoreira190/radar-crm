10ce40ddcc5faa8a527f2f81e89937f98d6dc894
beatrizmoreira190 <beatrizmoreira190@gmail.com>
2026-09-16 20:52:46 -0300

    Mount publisher UX hierarchy

--- app/crm/editoras/[id]/layout.js
+++ app/crm/editoras/[id]/layout.js
@@ -1,7 +1,8 @@
 import PublisherMaterialsMount from '@/components/PublisherMaterialsMount';
 import PublisherMeetingsMount from '@/components/PublisherMeetingsMount';
 import PublisherRadarDetailMount from '@/components/PublisherRadarDetailMount';
+import PublisherUxHierarchyMount from '@/components/PublisherUxHierarchyMount';
 
 export default function PublisherDetailLayout({children}){
-  return <>{children}<PublisherMeetingsMount/><PublisherMaterialsMount/><PublisherRadarDetailMount/></>;
+  return <>{children}<PublisherMeetingsMount/><PublisherMaterialsMount/><PublisherRadarDetailMount/><PublisherUxHierarchyMount/></>;
 }
