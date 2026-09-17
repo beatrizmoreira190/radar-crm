0db067f32084a8978bd942c9d02c71f63d30295a
beatrizmoreira190 <beatrizmoreira190@gmail.com>
2026-09-16 21:07:10 -0300

    Mount polished secondary meeting actions

--- app/crm/editoras/[id]/layout.js
+++ app/crm/editoras/[id]/layout.js
@@ -2,7 +2,8 @@ import PublisherMaterialsMount from '@/components/PublisherMaterialsMount';
 import PublisherMeetingsMount from '@/components/PublisherMeetingsMount';
 import PublisherRadarDetailMount from '@/components/PublisherRadarDetailMount';
 import PublisherUxHierarchyMount from '@/components/PublisherUxHierarchyMount';
+import PublisherMeetingActionsPolish from '@/components/PublisherMeetingActionsPolish';
 
 export default function PublisherDetailLayout({children}){
-  return <>{children}<PublisherMeetingsMount/><PublisherMaterialsMount/><PublisherRadarDetailMount/><PublisherUxHierarchyMount/></>;
+  return <>{children}<PublisherMeetingsMount/><PublisherMaterialsMount/><PublisherRadarDetailMount/><PublisherUxHierarchyMount/><PublisherMeetingActionsPolish/></>;
 }
