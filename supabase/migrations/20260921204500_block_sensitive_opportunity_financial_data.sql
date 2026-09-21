51d6cc48e0541c24d9e1912a91363e44cfd021c1
beatrizmoreira190 <beatrizmoreira190@gmail.com>
2026-09-21 17:45:16 -0300

    db: remover nome provisório da migration

--- supabase/migrations/20260921204500_block_sensitive_opportunity_financial_data.sql
+++
@@ -1,4 +0,0 @@
-alter table public.opportunities
-  drop constraint if exists opportunities_no_sensitive_financial_data_chk,
-  add constraint opportunities_no_sensitive_financial_data_chk
-    check (estimated_value is null and probability is null);
